a558da848279d5c49176f23c4bd9030e1726cb3a
dduxx <nmagee@unb.ca>
2018-02-07 12:54:41 -0400

    changes to unb resources access

---
+++ app/src/main/java/ca/unb/cs/cs2063g8/birdcall/ugrad/Description.java
@@ -0,0 +1,100 @@
+package ca.unb.cs.cs2063g8.birdcall.ugrad;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import ca.unb.cs.cs2063g8.birdcall.web.UNBAccess;
+
+/**
+ * Created by nmagee on 07/02/18.
+ */
+
+public class Description {
+    private final String DESCRIPTION_SELECTOR =
+            "#mcontent > table.ugradcal.large-only > tbody > tr > td > course_description > p";
+    private final String PREREQ_SELECTOR = "#mcontent > table.ugradcal.large-only > tbody > tr > td > course_prereq > p";
+    private URL descriptionUrl;
+    private String description;
+    private String prereqs;
+    private String response;
+
+    public Description(String url) throws MalformedURLException{
+        this(new URL(url));
+    }
+
+    public Description(URL url){
+        this.descriptionUrl = descriptionUrl;
+    }
+
+    public void setDescription(){
+        if(response == null) {
+            setResponse();
+        }
+
+        Document doc = Jsoup.parse(response);
+        Elements elements = doc.select(DESCRIPTION_SELECTOR);
+        this.description = elements.get(0).ownText();
+    }
+
+    public void setPrereqs(){
+        if(response == null) {
+            setResponse();
+        }
+        Document doc = Jsoup.parse(response);
+        Elements elements = doc.select(PREREQ_SELECTOR);
+        this.prereqs = elements.get(0).ownText();
+    }
+
+    public String getDescription(){
+        if(description == null){
+            setDescription();
+        }
+        return this.description;
+    }
+
+    public String getPrereqs(){
+        if(prereqs == null){
+            setPrereqs();
+        }
+        return this.prereqs;
+    }
+
+    public URL getDescriptionUrl(){
+        return this.descriptionUrl;
+    }
+
+    public void setDescriptionUrl(URL url){
+        this.descriptionUrl = url;
+    }
+
+    public void setDescriptionUrl(String url) throws MalformedURLException {
+        this.setDescriptionUrl(new URL(url));
+    }
+
+    public void setResponse(){
+        Map<String, String> params = new HashMap<>();
+        String response = UNBAccess.getResponse(params, descriptionUrl, UNBAccess.Expected.HTML);
+        this.response = response;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Description that = (Description) o;
+
+        return descriptionUrl.equals(that.descriptionUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return descriptionUrl.hashCode();
+    }
+}
---
+++ app/src/main/java/ca/unb/cs/cs2063g8/birdcall/web/UNBAccess.java
@@ -0,0 +1,116 @@
+package ca.unb.cs.cs2063g8.birdcall.web;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+
+/**
+ * @author nmagee
+ * date: 2018-02-04
+ * Uses java's http and url implementation to pass requests to a server and return responses
+ *     this is meant to be a helper class and cannot be instantiated. use this only as a uniform
+ *     way of gathering information from UNB's resources.
+ *
+ * these curl commands were used to gather data from UNB's timetables and faculty list:
+ *     curl -X POST -F 'level=UG' http://es.unb.ca/apps/timetable/ajax/get-subjects.cgi
+ *
+ *     curl -X POST -F 'action=index' -F 'noncredit=0' -F 'term=2018/WI' -F 'level=UG'
+ *         -F 'subject=ALLSUBJECTS' -F 'location=FR' -F 'format=CLASS'
+ *         http://es.unb.ca/apps/timetable/index.cgi
+ */
+
+public class UNBAccess {
+    private final static String TAG = "UNBAccess";
+    private final static String DEFAULT_ENCODING = "UTF-8";
+
+    private UNBAccess(){
+        //make it so you cannot instantiate this class
+    }
+
+    /**
+     *
+     * @param formParams parameters sent in http request from the form on the unb timetables site
+     * @param url the url to send the request
+     * @return a string response from the server. could be json or html depending on the
+     *     response. if there was an issue hitting the server this will return null
+     */
+    public static String getResponse(Map<String, String> formParams, URL url, Expected returnType){
+        try {
+            Log.i(TAG, "building form entries");
+
+            StringBuilder formData = new StringBuilder();
+
+            for (Map.Entry<String, String> param : formParams.entrySet()) {
+                if (formData.length() != 0) {
+                    formData.append('&');
+                }
+                formData.append(URLEncoder.encode(param.getKey(), DEFAULT_ENCODING));
+                formData.append('=');
+                formData.append(URLEncoder.encode(param.getValue(), DEFAULT_ENCODING));
+            }
+
+            byte[] formBytes = formData.toString().getBytes(DEFAULT_ENCODING);
+
+            Log.i(TAG, "building HTTP request header");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", String.valueOf(formBytes.length));
+            conn.setDoOutput(true); //allows for sending of request body info. ie: form data
+
+            Log.i(TAG, "sending form request body");
+            conn.getOutputStream().write(formBytes);
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream(), DEFAULT_ENCODING));
+
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            if(returnType == Expected.JSON){
+                while ((line = reader.readLine()) != null){
+                    response.append(line);
+                }
+            }
+            else{
+                //the parsable html contains a lot of scripts and other elements that we do not
+                //need. this will hopefully reduce the memory foot print by only copying what is
+                //within table elements
+                boolean tableOpen = false;
+                while ((line = reader.readLine()) != null){
+                    if(line.contains("<table") && !line.contains("id=\"term-summary\"")){
+                        tableOpen = true;
+                    }
+
+                    if(tableOpen){
+                        response.append(line);
+                    }
+
+                    if(line.contains("</table>")){
+                        tableOpen = false;
+                    }
+                }
+            }
+
+            //strips whitespace between tagged elements for readability in logging
+            return response.toString().replaceAll(">\\s*<", "><");
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, "encoding: " + DEFAULT_ENCODING + " is not supported", e);
+            return null;
+        } catch (IOException e){
+            Log.e(TAG, "unable to open url connection at: " + url, e);
+            return null;
+        }
+    }
+
+    public enum Expected {
+        JSON,
+        HTML
+    }
+}
