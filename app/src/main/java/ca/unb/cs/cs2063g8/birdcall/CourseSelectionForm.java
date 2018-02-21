76e02b8f58cb16f8fef78238baf2c17e2e3a270a
dduxx <nmagee@unb.ca>
2018-02-21 10:26:11 -0400

    setting up the drop down menus

--- app/src/main/java/ca/unb/cs/cs2063g8/birdcall/CourseSelectionForm.java
+++ app/src/main/java/ca/unb/cs/cs2063g8/birdcall/CourseSelectionForm.java
@@ -1,21 +1,26 @@
 package ca.unb.cs.cs2063g8.birdcall;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.StrictMode;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.unb.cs.cs2063g8.birdcall.ugrad.Course;
 import ca.unb.cs.cs2063g8.birdcall.ugrad.Description;
 import ca.unb.cs.cs2063g8.birdcall.ugrad.Faculty;
-import android.util.Log;
+import ca.unb.cs.cs2063g8.birdcall.ugrad.Location;
 
-/**
- * this is the curl command to get all course information:
- *
- */
+import android.text.Html;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
 public class CourseSelectionForm extends AppCompatActivity {
     private static final String TAG = "CourseSelectionForm";
 
@@ -23,16 +28,44 @@ public class CourseSelectionForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_form);
-        //TODO: this is super bad. do not do this. fix this later once testing is done. should
-        //run on another thread.
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.RED));
+        actionBar.setTitle(Html.fromHtml("<font color='"
+                + Color.BLACK + "'>BirdCall</font>"));
 
-        List<Course> courses = Course.getCourseList();
-        Log.i(TAG, "" + courses.size());//just seeing if the list is empty
-        Description description = courses.get(0).getDescription();//get a description for a course
-        Log.i(TAG, description.getPrereqs());//see if it has prereqs
-        Log.i(TAG, description.getDescription());//see if it has description
+        populateLocationSpinner();
+        populateLevelSpinner();
+    }
+
+    private void populateLevelSpinner(){
+        Spinner levelSpinner = findViewById(R.id.level_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.level_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        levelSpinner.setAdapter(adapter);
+    }
+
+    private void populateLocationSpinner(){
+        List<Location> locations = Location.getAllLocations();
+
+        ArrayList<String> locationNames = new ArrayList<>();
+
+        for(Location l : locations){
+            locationNames.add(l.getName());
+        }
+        Spinner locationSpinner = findViewById(R.id.location_spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, locationNames);
+
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        locationSpinner.setAdapter(adapter);
+    }
+
+    private void populateFacultySpinner(){
 
     }
 }
